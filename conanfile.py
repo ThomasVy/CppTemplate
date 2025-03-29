824d597773a4655ff9fb8dafb92e77e31fec5cb9
Thomas Vy <vythomas97@gmail.com>
2025-03-29 01:25:45 -0600

    moves imgui to imgui_src
    and create minimal src

--- conanfile.py
+++ conanfile.py
@@ -13,9 +13,15 @@ class MyProjectConan(ConanFile):
     def requirements(self):
         # Runtime requirements
         self.requires("fmt/11.0.1")
-        self.requires("imgui/1.91.8-docking")
-        self.requires("glfw/3.4")
-        self.requires("glew/2.2.0")
+        # self.requires("imgui/1.91.8-docking")
+        # self.requires("glfw/3.4")
+        # self.requires("glew/2.2.0")
+
+    # def generate(self):
+    #     copy(self, "*glfw*", os.path.join(self.dependencies["imgui"].package_folder,
+    #          "res", "bindings"), os.path.join(self.source_folder, "src/bindings"))
+    #     copy(self, "*opengl3*", os.path.join(self.dependencies["imgui"].package_folder,
+    #          "res", "bindings"), os.path.join(self.source_folder, "src/bindings"))
 
     def build_requirements(self):
         # Build requirements
@@ -30,8 +36,3 @@ class MyProjectConan(ConanFile):
         cmake.configure(variables={"CMAKE_BUILD_TYPE": str(self.settings.build_type)})
         cmake.build()
 
-    def generate(self):
-        copy(self, "*glfw*", os.path.join(self.dependencies["imgui"].package_folder,
-             "res", "bindings"), os.path.join(self.source_folder, "src/bindings"))
-        copy(self, "*opengl3*", os.path.join(self.dependencies["imgui"].package_folder,
-             "res", "bindings"), os.path.join(self.source_folder, "src/bindings"))
