a05d050bd9b2b110bad003a4cc376f3b51870749
Thomas Vy <vythomas97@gmail.com>
2026-01-23 03:23:25 -0700

    updates template

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,10 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeToolchain
 
 class CppTemplate(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "VirtualBuildEnv", "CMakeToolchain"
+    generators = "CMakeDeps", "VirtualBuildEnv"
 
     def layout(self):
         cmake_layout(self)
@@ -12,6 +13,9 @@ class CppTemplate(ConanFile):
          self.options["fmt"].shared = False
 
     def imports(self):
+       tc = CMakeToolchain(self)
+       tc.user_presets_path = False # This disables the generation
+       tc.generate()
        self.copy("*.dll", "", "bin")
        self.copy("*.dylib", "", "lib")
 
